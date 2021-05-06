8923a0307bed881e15ca3e77b8f833d30c8a9bd4
Luana <ls.luanastephany@gmail.com>
2021-05-06 10:05:31 -0300

    finalizando script

--- scripts.js
+++ scripts.js
@@ -7,19 +7,18 @@ const Modal = {
    }
 }
 
+const Storage = {
+   get() {
+      return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+   },
+
+   set(transactions) {
+      localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+   }
+}
+
 const Transaction = {
-   all: [
-      {
-         description: 'Luz',
-         amount: -50000,
-         date: '23/01/2021',
-      },
-      {
-         description: 'Freelance',
-         amount: 150000,
-         date: '23/01/2021',
-      },
-   ],
+   all: Storage.get(),
 
    add(transaction) { //add transações
       Transaction.all.push(transaction)
@@ -59,12 +58,13 @@ const DOM = {
 
    addTransaction(transaction, index) {
       const tr = document.createElement('tr')
-      tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+      tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+      tr.dataset.index = index
 
       DOM.transactionsContainer.appendChild(tr)
 
    },
-   innerHTMLTransaction(transaction) {
+   innerHTMLTransaction(transaction, index) {
       const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
       const amount = Utils.formatCurrency(transaction.amount)
@@ -74,7 +74,7 @@ const DOM = {
       <td class="${CSSclass}"> ${amount}</td>
       <td class="date"> ${transaction.date}</td>
       <td>
-        <img src="./assets/minus.svg">
+        <img onclick="Transaction.remove(${index})" src="./assets/minus.svg">
       </td>
     `
 
@@ -179,10 +179,13 @@ const Form = {
 const App = {
    init() {
       //retornar todos os itens sem precisar ficar repetindo cada um aqui
-      Transaction.all.forEach((transaction) => {
-         DOM.addTransaction(transaction)
+      Transaction.all.forEach((transaction, index) => {
+         DOM.addTransaction(transaction, index)
       })
+
       DOM.updateBalance()
+
+      Storage.set(Transaction.all)
    },
 
    reload() {
